ab0051935f322ecde149108f6332a9535133a405
Jurn Wubben <jurn.wubben@ing.com>
2024-08-14 19:41:46 +0200

    Added support for iconify in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import { addDynamicIconSelectors } from '@iconify/tailwind';
 import daisyui from 'daisyui';
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -5,5 +6,5 @@ export default {
 	theme: {
 		extend: {}
 	},
-	plugins: [daisyui]
+	plugins: [daisyui, addDynamicIconSelectors()]
 };
