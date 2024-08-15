a616f8979b4a1139933f019a06a34e592433f7ec
= <=>
2024-08-15 23:55:29 +0200

    Started work on profile options, still looks ugly as hell

--- src/lib/Options/OProfile.svelte
+++ src/lib/Options/OProfile.svelte
@@ -1,2 +1,26 @@
-<h3>Coming soon&trade;</h3>
-<h3>Proifle</h3>
+<script lang="ts">
+	import { profiles } from '../Stores/Profiles.svelte';
+</script>
+
+<h3>Profile</h3>
+<ul class="menu bg-base-200 rounded-box w-56">
+	{#each Object.keys(profiles.profiles) as p}
+		<li>
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<!-- svelte-ignore a11y_missing_attribute -->
+			<a class:active={p === profiles.activeProfile} onclick={_ => profiles.activeProfile = p} class="flex flex-row">
+                <span class="flex-grow">{p}</span>
+                <div class="divider divider-horizontal m-0"></div>
+                <div class="flex-none join">
+                    <button class="btn btn-xs btn-square btn-info join-item">
+                        <span class="icon-[mdi--rename-box] w-[80%] h-[80%]"></span>
+                    </button> 
+                    <button class="btn btn-xs btn-square disabled:bg-base-100 disabled:text-base-content btn-error join-item" disabled={p !== profiles.activeProfile}>
+                        <span class="icon-[material-symbols--delete] w-[80%] h-[80%]"></span>
+                    </button> 
+                </div>
+			</a>
+		</li>
+	{/each}
+</ul>
