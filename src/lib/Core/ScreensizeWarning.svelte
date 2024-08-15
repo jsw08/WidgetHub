fbe06c5e7925289b6621e77aa58a3eccc9f69853
Jurn Wubben <jurn.wubben@ing.com>
2024-08-15 20:18:18 +0200

    Formatting

--- src/lib/Core/ScreensizeWarning.svelte
+++ src/lib/Core/ScreensizeWarning.svelte
@@ -18,16 +18,33 @@
 	});
 </script>
 
-
 <dialog class="modal modal-bottom sm:modal-middle bg-base-100" {open}>
-  <div class="modal-box bg-error text-error-content">
-    <h3 class="text-lg font-bold">Invalid window size</h3>
-    <p class="py-4">Resize the window to make it larger or press the reset button to reset your profile or press Resize to recalculate .</p>
-    <div class="modal-action">
-      <div class="join">
-        <button class="btn join-item" onclick={_ => {localStorage.clear(); location.reload();}}>Reset</button>
-        <button class="btn btn-primary join-item" onclick={_ => profiles.profile.gridSize.boxSize = calcBoxSize(innerWidth, innerHeight, gridSize.rows, gridSize.cols)}>Resize</button>
-      </div>
-    </div>
-  </div>
+	<div class="modal-box bg-error text-error-content">
+		<h3 class="text-lg font-bold">Invalid window size</h3>
+		<p class="py-4">
+			Resize the window to make it larger or press the reset button to reset your profiles or press
+			Resize to recalculate.
+		</p>
+		<div class="modal-action">
+			<div class="join">
+				<button
+					class="btn join-item"
+					onclick={(_) => {
+						localStorage.clear();
+						location.reload();
+					}}>Reset</button
+				>
+				<button
+					class="btn btn-primary join-item"
+					onclick={(_) =>
+						(profiles.profile.gridSize.boxSize = calcBoxSize(
+							innerWidth,
+							innerHeight,
+							gridSize.rows,
+							gridSize.cols
+						))}>Resize</button
+				>
+			</div>
+		</div>
+	</div>
 </dialog>
