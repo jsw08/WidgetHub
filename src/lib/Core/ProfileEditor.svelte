6d41d76be2cd48be6c568cd0c2c215bf4a86be79
= <=>
2024-08-17 15:38:16 +0200

    Created dialog for profileeditor

--- src/lib/Core/ProfileEditor.svelte
+++ src/lib/Core/ProfileEditor.svelte
@@ -1,4 +1,26 @@
 <script lang="ts">
+	import { profiles } from '../Stores/Profiles.svelte';
 
+	type Props = {
+		profileName: string;
+	};
+	let { profileName }: Props = $props();
+	let profile = $derived(profiles.profiles[profileName]);
 </script>
 
+{#if profile}
+	<dialog class="modal modal-bottom sm:modal-middle" open>
+		<div class="modal-box">
+			<h3 class="text-lg font-bold">Editing {profileName}</h3>
+
+			<div class="modal-action flex flex-row"></div>
+		</div>
+	</dialog>
+{:else}
+	<dialog class="modal modal-bottom sm:modal-middle" open>
+		<div class="modal-box">
+			<h3 class="text-lg font-bold">Profile not found...</h3>
+			<div class="modal-action flex flex-row"></div>
+		</div>
+	</dialog>
+{/if}
