95f7250fb2f3e7aca8f5f12aabc194defd94a436
= <=>
2024-08-17 16:54:44 +0200

    Fixed setup not setting name when using enter key

--- src/lib/Setup/Profile.svelte
+++ src/lib/Setup/Profile.svelte
@@ -3,6 +3,7 @@
 	import Paginator from './Paginator.svelte';
 
 	let name = ""
+	const setName = () => setup.profileName = name;
 </script>
 
 <h3 class="text-lg font-bold">Profiles</h3>
@@ -11,12 +12,12 @@
 	enter a name for your first profile below.
 </p>
 
-<Paginator disabled={!name.length} next={() => setup.profileName = name}>
+<Paginator disabled={!name.length} onnext={setName}>
 	<input
 		bind:value={name}
 		type="text"
 		placeholder="E.g: 'default'"
 		class="input input-bordered flex-grow min-w-0"
-		onkeypress={(e) => (e.key === 'Enter' ? setup.index++ : void 0)}
+		onkeypress={(e) => (e.key === 'Enter' ? (() => {setup.index++; setName()})() : void 0)}
 	/>
 </Paginator>
